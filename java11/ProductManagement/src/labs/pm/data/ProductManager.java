8e82ec98d97359f39cb25291b4e6cd3f03df56ba
Sertaç Demir <sertacdemir@Sertacs-MacBook-Pro.local>
2023-01-18 10:40:06 +0100

    Arrays and Loops

--- java11/ProductManagement/src/labs/pm/data/ProductManager.java
+++ java11/ProductManagement/src/labs/pm/data/ProductManager.java
@@ -13,6 +13,7 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -24,7 +25,7 @@ import java.util.ResourceBundle;
 public class ProductManager {
 
     private Product product;
-    private Review review;
+    private Review[] reviews = new Review[5];
     private Locale locale;
     private ResourceBundle resources;
     private DateTimeFormatter dateFormat;
@@ -47,8 +48,22 @@ public class ProductManager {
     }
 
     public Product reviewProduct(Product product, Rating rating, String comments){
-        review = new Review(rating,comments);
-        this.product = product.applyRating(rating);
+        if (reviews[reviews.length-1] != null){
+            reviews = Arrays.copyOf(reviews,reviews.length+5);
+        }
+        int sum = 0, i = 0;
+        boolean reviewed = false;
+
+        while (i < reviews.length && !reviewed){
+            if (reviews[i] == null){
+                reviews[i] = new Review(rating,comments);
+                reviewed = true;
+            }
+            sum += reviews[i].getRating().ordinal();
+            i++;
+        }
+//        review = new Review(rating,comments);
+        this.product = product.applyRating(Rateable.convert(Math.round((float) sum/i)));
         return this.product;
     }
 
@@ -60,14 +75,20 @@ public class ProductManager {
                 product.getRating().getStars(),
                 dateFormat.format(product.getBestBefore())));
         txt.append('\n');
-        if (review != null){
+        for (Review review : reviews){
+            if (review == null) {
+                break;
+            }
             txt.append(MessageFormat.format(resources.getString("review"),
                     review.getRating().getStars(),
                     review.getComments()));
-        } else {
-            txt.append(resources.getString("no.reviews"));
+            txt.append('\n');
         }
-        txt.append('\n');
+        if (reviews[0] == null) {
+            txt.append(resources.getString("no.reviews"));
+            txt.append('\n');
+        }
+
         System.out.println(txt);
     }
 }
